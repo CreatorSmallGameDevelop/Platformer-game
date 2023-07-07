089ada79aaeaf18d314908d5cff95d7bedef5b69
dongjun Noh <bluecode671@gmail.com>
2023-07-07 17:11:00 +0900

    캐릭터 움직임 추가

---
+++ Platformer Game/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed;
+
+    void Update()
+    {
+        // float horizontalInput = Input.GetAxisRaw("Horizontal");
+        // float verticalInput = Input.GetAxisRaw("Vertical");
+        // Vector3 moveTo = new Vector3(horizontalInput, verticalInput, 0f);
+        // transform.position += moveTo * moveSpeed * Time.deltaTime;
+
+
+
+        Vector3 moveTo = new Vector3(moveSpeed * Time.deltaTime, 0, 0);
+        if (Input.GetKey(KeyCode.LeftArrow)){
+            transform.position -= moveTo;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow)){
+            transform.position += moveTo;
+        }
+    }
+}
